351bdff8571282ccf9118bbfb85a6b2d22ba5d2d
Tommy Calvy <thomaslcalvy@gmail.com>
2019-06-06 15:12:31 -0500

    hanging legt

--- static/app.js
+++ static/app.js
@@ -9,10 +9,6 @@ var Mouse;
 var MouseConstraint;
 var engine;
 var world;
-//var stickHead;
-//var stickBody;
-//var stickLegRightUpper;
-//var stickLegLeftUpper;
 var ground;
 var leg;
 
@@ -20,7 +16,7 @@ var leg;
 
 function setup() {
   var canvas = createCanvas(600, 600);
-  // module aliases
+
   Engine = Matter.Engine;
   World = Matter.World;
   Bodies = Matter.Bodies;
@@ -31,46 +27,12 @@ function setup() {
   engine = Engine.create();
   world = engine.world;
 
-  //stickHead = new StickHead(300, 15, 50);
-  //stickBody = new Box(300, 100, 50, 200);
-  //stickLegRightUpper = new Box(300, 185, 50, 150);
-  //stickLegLeftUpper = new Box(300, 185, 50, 150);
+
 
   ground = new Boundary(width / 2, 575, width, 50);
 
   World.add(world, ground);
-  /*
-  var options1 = {
-    bodyA: stickHead.body,
-    bodyB: stickBody.body,
-    pointB: { x: 0, y: 85 },
-    length: 0,
-    stiffness: 1
-  }
-  var headToBodyConstraint = Constraint.create(options1);
 
-  var options2 = {
-    bodyA: stickBody.body,
-    pointA: { x: 0, y: -85 },
-    bodyB: stickLegRightUpper.body,
-    pointB: { x: 0, y: 85 },
-    length: 0,
-    stiffness: 1
-  }
-  var bodyToRightUpperLegConstraint = Constraint.create(options2);
-
-  var options3 = {
-    bodyA: stickBody.body,
-    pointA: { x: 0, y: -85 },
-    bodyB: stickLegLeftUpper.body,
-    pointB: { x: 0, y: 85 },
-    length: 0,
-    stiffness: 1
-  }
-  var bodyToLeftUpperLegConstraint = Constraint.create(options3);
-
-  World.add(world, [headToBodyConstraint, bodyToRightUpperLegConstraint, bodyToLeftUpperLegConstraint]);
-  */
   leg = new StickLeg(400, 200);
   World.add(world, leg);
 
@@ -86,36 +48,22 @@ function setup() {
   }
   mouseConstraint = MouseConstraint.create(engine, options4);
   World.add(world, mouseConstraint);
-  //console.log(stickBody);
   Engine.run(engine);
 }
 
 function draw() {
   background(200);
-  //ground.show();
   ground.show();
-  //stickHead.show();
-  //stickBody.show();
-  //stickLegRightUpper.show();
-  //stickLegLeftUpper.show();
   leg.show();
-  //console.log(box1.pos.y)
-}
-/*
-class Line {
-  constructor(v1, v2) {
-    this.x1 = v1[0];
-    this.y1 = v1[1];
-    this.x2 = v2[0];
-    this.y2 = v2[1];
-  }
-
-  show() {
-    line(this.x1, this.y1, this.x2, this.y2);
-  }
 }
 
-class Rectangle {
-  constructor()
+function keyPressed() {
+  console.log("Key Pressed!!");
+  if (keyCode === o) {
+    console.log("O key pressed");
+    leg.extend();
+  } else if (keyCode === p) {
+    console.log("P key pressed");
+    leg.contract();
+  }
 }
-*/
--- static/stickleg.js
+++ static/stickleg.js
@@ -1,31 +1,55 @@
 function StickLeg(x, y) {
   this.upperLegVertices = [ {x: 0, y: 0}, {x: 0, y: 100}, {x: 30, y: 200}, {x: 100, y: 190}, {x: 60, y: 0} ];
-  var options = {
-      collisionFilter: {
-            group: -2,
-      }
-    }
-  this.upperLeg = Bodies.fromVertices(x, y, this.upperLegVertices, options);
+  this.upperLeg = Bodies.fromVertices(x, y, this.upperLegVertices);
 
-  this.lowerLeg = Bodies.rectangle(30, 201, 50, 200, options);
+  this.lowerLeg = Bodies.rectangle(30, 201, 60, 200);
 
-  var options = {
+  var option1 = {
     bodyA: this.upperLeg,
-    pointA: { x: 30, y: 200 },
+    pointA: { x: -15, y: 99 },
     bodyB: this.lowerLeg,
-    pointB: { x: 30, y: 201 },
-    length: 1,
-    stiffness: 0.9
+    pointB: { x: -26, y: -100 },
+    length: 0,
+    stiffness: 1
   }
-  var legConstraint = Constraint.create(options);
+  this.legConstraint1 = Constraint.create(option1);
 
-  World.add(world, [this.upperLeg, this.lowerLeg, legConstraint]);
+  var option2 = {
+    bodyA: this.upperLeg,
+    pointA: { x: -15, y: -85},
+    pointB: { x: 300, y: 0},
+    length: 100,
+    stiffness: 0.7
+  }
+  this.legConstraint2 = Constraint.create(option2);
+
+  var options10 = {
+    bodyA: this.upperLeg,
+    pointA: { x: -100, y: -100},
+    bodyB: this.lowerLeg,
+    pointB: { x: - 100, y: 0},
+    length: 300,
+    stiffness: 0.1
+  }
+  this.moveCon = Constraint.create(options10);
+
+  World.add(world, [this.upperLeg, this.lowerLeg, this.legConstraint1, this.legConstraint2, this.moveCon]);
 
   this.show = function() {
     var upperLegPos = this.upperLeg.vertices[0];
     var upperLegAngle = this.upperLeg.angle;
     var lowerLegPos = this.lowerLeg.position;
     var lowerLegAngle = this.lowerLeg.angle;
+
+    var uLegCentroid = this.upperLeg.position;
+    var uLegOffset = this.legConstraint1.pointA;
+    var lLegOffset = this.legConstraint1.pointB;
+
+    var uLegOffset2 = this.legConstraint2.pointA;
+    var fixedOffset = this.legConstraint2.pointB;
+
+    var uMoveCon = this.moveCon.pointA;
+    var lMoveCon = this.moveCon.pointB;
 
     push();
     translate(upperLegPos.x, upperLegPos.y);
@@ -42,7 +66,28 @@ function StickLeg(x, y) {
     translate(lowerLegPos.x, lowerLegPos.y);
     rotate(lowerLegAngle);
     fill(0);
+    rectMode(CENTER);
     rect(0, 0, 50, 200);
     pop();
+
+    push();
+    stroke(0, 255, 0);
+    line(uLegCentroid.x + uLegOffset.x, uLegCentroid.y + uLegOffset.y, lowerLegPos.x + lLegOffset.x, lowerLegPos.y + lLegOffset.y);
+    line(fixedOffset.x, fixedOffset.y, uLegCentroid.x + uLegOffset2.x, uLegCentroid.y + uLegOffset2.y);
+    pop();
+
+    push();
+    stroke(255, 0, 0);
+    line(uLegCentroid.x + uMoveCon.x, uLegCentroid.y + uMoveCon.y, lowerLegPos.x + uMoveCon.x, lowerLegPos.y + uMoveCon.y);
+    pop();
   }
+
+  this.extend = function() {
+    this.moveCon.length += 1;
+  }
+
+  this.contract = function() {
+    this.moveCon.length -= 1;
+  }
+
 }
